384d7183a095ab3a5a21e8961ceeb1b782ec11fd
Nathan Muir <ndmuir@gmail.com>
2014-07-27 20:57:19 +1000

    Added new hooks `onEachRun` and `onBeforeSwitch`.  Modified `waitOn` to inherit from the hierarchy of RouteControllers.
    `onEachRun` - Works similar to `onRun` in that it is called before `waitOn` hooks - however it will be run on each new page reload (useful for permission checks)
    `onBeforeSwitch` - Works similar to `onBeforeAction` except it it called before the current layout changes / is destroyed. Calling `pause()` in this hook will keep you on the current page.
    `waitOn` - now uses the same "hook inheritance" as regular action hooks.
    
    Hooks are compiled by common function `RouteController#_collectHooks`

--- package.js
+++ package.js
@@ -3,43 +3,35 @@ Package.describe({
   version: '0.9.1'
 });
 
-Package.on_use(function (api) {
-  api.use('reactive-dict@1.0.0', ['client', 'server']);
-  api.use('deps@1.0.0', ['client', 'server']);
-  api.use('underscore@1.0.0', ['client', 'server']);
-  api.use('ejson@1.0.0', ['client', 'server']);
-  api.use('jquery@1.0.0', 'client');
-  api.use('webapp@1.0.0', 'server');
+Package.onUse(function (api) {
 
-  // default ui manager
-  // use unordered: true becuase of circular dependency
+  api.versionsFrom('METEOR-CORE@0.9.0-rc12');
 
-  // for helpers
-  api.use('ui@1.0.0', 'client');
- 
+  api.use('reactive-dict', ['client', 'server']);
+  api.use('deps', ['client', 'server']);
+  api.use('underscore', ['client', 'server']);
+  api.use('ejson', ['client', 'server']);
+  api.use('jquery', 'client');
+  api.use('webapp', 'server');
+  api.use('ui', 'client');
+
   // gives us dynamic layouts
   api.use('iron:layout@0.3.0');
 
-  // in case they have the old version that was
-  // automigrated from atmosphere.
-  api.use('cmather:iron-router@0.8.2', {weak: true});
+  api.addFiles('lib/utils.js', ['client', 'server']);
+  api.addFiles('lib/route.js', ['client', 'server']);
+  api.addFiles('lib/route_controller.js', ['client', 'server']);
+  api.addFiles('lib/router.js', ['client', 'server']);
 
-  api.add_files('lib/utils.js', ['client', 'server']);
-  api.add_files('lib/route.js', ['client', 'server']);
-  api.add_files('lib/route_controller.js', ['client', 'server']);
-  api.add_files('lib/router.js', ['client', 'server']);
+  api.addFiles('lib/client/location.js', 'client');
+  api.addFiles('lib/client/router.js', 'client');
+  api.addFiles('lib/client/wait_list.js', 'client');
+  api.addFiles('lib/client/hooks.js', 'client');
+  api.addFiles('lib/client/route_controller.js', 'client');
+  api.addFiles('lib/client/ui/helpers.js', 'client');
 
-  api.add_files('lib/client/location.js', 'client');
-  api.add_files('lib/client/router.js', 'client');
-  api.add_files('lib/client/wait_list.js', 'client');
-  api.add_files('lib/client/hooks.js', 'client');
-  api.add_files('lib/client/route_controller.js', 'client');
-  api.add_files('lib/client/ui/helpers.js', 'client');
-
-  api.add_files('lib/server/route_controller.js', 'server');
-  api.add_files('lib/server/router.js', 'server');
-
-  api.add_files('lib/version_conflict_error.js');
+  api.addFiles('lib/server/route_controller.js', 'server');
+  api.addFiles('lib/server/router.js', 'server');
 
   Npm.depends({connect: '2.9.0'});
 
@@ -53,27 +45,27 @@ Package.on_use(function (api) {
   api.export('WaitList', 'client', {testOnly: true});
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use('iron:router', ['client', 'server']);
   api.use('tinytest', ['client', 'server']);
   api.use('test-helpers', ['client', 'server']);
   api.use('reactive-dict', ['client', 'server']);
   api.use('underscore');
 
-  api.add_files('test/test_helpers.js', ['client', 'server']);
+  api.addFiles('test/test_helpers.js', ['client', 'server']);
 
   // client and server
-  api.add_files('test/both/route.js', ['client', 'server']);
-  api.add_files('test/both/route_controller.js', ['client', 'server']);
-  api.add_files('test/both/router.js', ['client', 'server']);
-  api.add_files('test/both/utils.js', ['client', 'server']);
+  api.addFiles('test/both/route.js', ['client', 'server']);
+  api.addFiles('test/both/route_controller.js', ['client', 'server']);
+  api.addFiles('test/both/router.js', ['client', 'server']);
+  api.addFiles('test/both/utils.js', ['client', 'server']);
 
   // server only
-  api.add_files('test/server/router.js', 'server');
+  api.addFiles('test/server/router.js', 'server');
 
   // client only
-  api.add_files('test/client/mocks.js', 'client');
-  api.add_files('test/client/router.js', 'client');
-  api.add_files('test/client/route_controller.js', 'client');
-  api.add_files('test/client/wait_list.js', 'client');
+  api.addFiles('test/client/mocks.js', 'client');
+  api.addFiles('test/client/router.js', 'client');
+  api.addFiles('test/client/route_controller.js', 'client');
+  api.addFiles('test/client/wait_list.js', 'client');
 });
